fb780e4016e01c4839562809282462f27697bc87
Furkan Karayel <furkan.karayel@outlook.de>
2024-06-23 23:28:00 +0200

    structs used for processing the output of a Fen search request properly over scoutfish main route

--- scoutfish/scoutfish.go
+++ scoutfish/scoutfish.go
@@ -1,8 +1,11 @@
 package scoutfish
 
 import (
+	"encoding/json"
 	"engine"
 	"engine/helper"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -37,14 +40,46 @@ func (s *Scoutfish) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		engine.Respond(w, r, http.StatusNotFound, "scoutfish path not found")
 	}
 }
-func (u *Scoutfish) home(w http.ResponseWriter, r *http.Request) {
-	jsonString := `{"sub-fen": "r1bqkb1r/pppp1ppp/2n2n2/4p1N1/2B1P3/8/PPPP1PPP/RNBQK2R b KQkq - 0 1"}`
 
-	result, err := helper.NewRunner("../bin/scoutfish/src/scoutfish").Run("scout ../pgn/LumbrasGigaBase-1899.scout", jsonString)
+type Match struct {
+	Ofs int   `json:"ofs"`
+	Ply []int `json:"ply"`
+}
+
+// RunnerOutput represents the JSON structure of the command output
+type RunnerOutput struct {
+	Moves          int     `json:"moves"`
+	MatchCount     int     `json:"match count"`
+	MovesPerSecond int     `json:"moves/second"`
+	ProcessingTime int     `json:"processing time (ms)"`
+	Matches        []Match `json:"matches"`
+}
+
+type FenInput struct {
+	SubFen string `json:"sub-fen"`
+}
+
+func (u *Scoutfish) home(w http.ResponseWriter, r *http.Request) {
+	// jsonString := `{"sub-fen": "r1bqkb1r/pppp1ppp/2n2n2/4p1N1/2B1P3/8/PPPP1PPP/RNBQK2R b KQkq - 0 1"}`
+
+	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Printf("Command execution failed: %v\nOutput: %s", err, result)
+		http.Error(w, fmt.Sprintf("Failed to read request body: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	result, err := helper.NewRunner("../bin/scoutfish/src/scoutfish").Run("scout ../pgn/LumbrasGigaBase-1899.scout", string(body))
+	if err != nil {
 		engine.Respond(w, r, http.StatusBadRequest, err)
 		return
 	}
-	engine.Respond(w, r, http.StatusOK, result)
+
+	var output RunnerOutput
+
+	err = json.Unmarshal([]byte(result), &output)
+	if err != nil {
+		engine.Respond(w, r, http.StatusBadRequest, err)
+	}
+
+	engine.Respond(w, r, http.StatusOK, output)
 }
